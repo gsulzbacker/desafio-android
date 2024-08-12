1c14278e9a7d53a35f123d6b07cc854dba16ed55
gabriel.sulzbacker@gmail.com <gabriel.sulzbacker@SHOPPER.CORP>
2024-08-12 01:29:04 -0300

    Implementa MVVM no projeto

--- app/src/main/java/com/picpay/desafio/android/presentation/ui/UserListAdapter.kt
+++ app/src/main/java/com/picpay/desafio/android/presentation/ui/UserListAdapter.kt
@@ -1,9 +1,11 @@
-package com.picpay.desafio.android
+package com.picpay.desafio.android.presentation.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.picpay.desafio.android.UserListDiffCallback
+import com.picpay.desafio.android.data.model.User
 import com.picpay.desafio.android.databinding.ListItemUserBinding
 
 class UserListAdapter : RecyclerView.Adapter<UserListItemViewHolder>() {
